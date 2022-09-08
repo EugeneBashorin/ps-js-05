f3b38c84b7e328f1967c6945ddf1cdd2ba8f5319
EugeneBashorin <bashorineugene@gmail.com>
2022-09-08 18:30:17 +0300

    add 3 task
    promise generator

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+formData = document.querySelector(".form");
+
+formData.addEventListener("submit", submitForm);
+
+function submitForm(event){
+  event.preventDefault();
+
+  let delay = Number(document.querySelector('[name="delay"]').value);
+  const step = Number(document.querySelector('[name="step"]').value);
+  const amountData = Number(document.querySelector('[name="amount"]').value);
+
+  for(let position = 1; position <= amountData; position +=1){
+    createPromise({position, delay})
+    .then(({position, delay}) => Notify.success(`✅ Fulfilled promise ${position} in ${delay} ms`))
+    .catch(({position, delay}) => Notify.failure(`❌ Rejected promise ${position} in ${delay} ms`))
+    delay += step;
   }
+}
+
+function createPromise({position, delay}) {
+      const shouldResolve = Math.random() > 0.3;
+
+      return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (shouldResolve) {
+          // Fulfill
+        resolve({position, delay});
+      }else{
+          // Reject
+        reject({position, delay});
+      }  
+    }, delay);
+    
+    })
 }
